dc74950e995c3c697f1a1b3708b4683ba21e7adb
Carlos Agüero <caguero@osrfoundation.org>
2014-08-13 13:40:31 -0700

    Better documentation for Pack()/Unpack() methods.

--- src/Packet.cc
+++ src/Packet.cc
@@ -194,6 +194,7 @@ size_t SubscriptionMsg::GetMsgLength()
 //////////////////////////////////////////////////
 size_t SubscriptionMsg::Pack(char *_buffer)
 {
+  // Pack the header.
   size_t headerLen = this->GetHeader().Pack(_buffer);
   if (headerLen == 0)
     return 0;
@@ -207,9 +208,12 @@ size_t SubscriptionMsg::Pack(char *_buffer)
 
   _buffer += headerLen;
 
+  // Pack the topic length.
   size_t topicLength = this->topic.size();
   memcpy(_buffer, &topicLength, sizeof(topicLength));
   _buffer += sizeof(topicLength);
+
+  // Pack the topic.
   memcpy(_buffer, this->topic.data(), topicLength);
 
   return this->GetMsgLength();
@@ -218,12 +222,12 @@ size_t SubscriptionMsg::Pack(char *_buffer)
 //////////////////////////////////////////////////
 size_t SubscriptionMsg::UnpackBody(char *_buffer)
 {
-  // Read the topic length.
+  // Unpack the topic length.
   size_t topicLength;
   memcpy(&topicLength, _buffer, sizeof(topicLength));
   _buffer += sizeof(topicLength);
 
-  // Read the topic.
+  // Unpack the topic.
   this->topic = std::string(_buffer, _buffer + topicLength);
   _buffer += topicLength;
 
@@ -332,6 +336,7 @@ size_t AdvertiseBase::GetMsgLength()
 //////////////////////////////////////////////////
 size_t AdvertiseBase::Pack(char *_buffer)
 {
+  // Pack the header.
   size_t headerLen = this->GetHeader().Pack(_buffer);
   if (headerLen == 0)
     return 0;
@@ -345,26 +350,43 @@ size_t AdvertiseBase::Pack(char *_buffer)
 
   _buffer += headerLen;
 
+  // Pack the topic length.
   size_t topicLength = this->topic.size();
   memcpy(_buffer, &topicLength, sizeof(topicLength));
   _buffer += sizeof(topicLength);
+
+  // Pack the topic.
   memcpy(_buffer, this->topic.data(), topicLength);
   _buffer += topicLength;
+
+  // Pack the zeromq address length.
   size_t addrLength = this->addr.size();
   memcpy(_buffer, &addrLength, sizeof(addrLength));
   _buffer += sizeof(addrLength);
+
+  // Pack the zeromq address.
   memcpy(_buffer, this->addr.data(), addrLength);
   _buffer += addrLength;
+
+  // Pack the zeromq control address length.
   size_t ctrlLength = this->ctrl.size();
   memcpy(_buffer, &ctrlLength, sizeof(ctrlLength));
   _buffer += sizeof(ctrlLength);
+
+  // Pack the zeromq control address.
   memcpy(_buffer, this->ctrl.data(), ctrlLength);
   _buffer += ctrlLength;
+
+  // Pack the node UUID length.
   size_t nUuidLength = this->nUuid.size();
   memcpy(_buffer, &nUuidLength, sizeof(nUuidLength));
   _buffer += sizeof(nUuidLength);
+
+  // Pack the node UUID.
   memcpy(_buffer, this->nUuid.data(), nUuidLength);
   _buffer += nUuidLength;
+
+  // Pack the topic scope.
   memcpy(_buffer, &this->scope, sizeof(this->scope));
 
   return this->GetMsgLength();
@@ -373,43 +395,43 @@ size_t AdvertiseBase::Pack(char *_buffer)
 //////////////////////////////////////////////////
 size_t AdvertiseBase::UnpackBody(char *_buffer)
 {
-  // Read the topic length.
+  // Unpack the topic length.
   size_t topicLength;
   memcpy(&topicLength, _buffer, sizeof(topicLength));
   _buffer += sizeof(topicLength);
 
-  // Read the topic.
+  // Unpack the topic.
   this->topic = std::string(_buffer, _buffer + topicLength);
   _buffer += topicLength;
 
-  // Read the address length.
+  // Unpack the zeromq address length.
   size_t addrLength;
   memcpy(&addrLength, _buffer, sizeof(addrLength));
   _buffer += sizeof(addrLength);
 
-  // Read the address.
+  // Unpack the zeromq address.
   this->addr = std::string(_buffer, _buffer + addrLength);
   _buffer += addrLength;
 
-  // Read the control address length.
+  // Unpack the zeromq control address length.
   size_t ctrlLength;
   memcpy(&ctrlLength, _buffer, sizeof(ctrlLength));
   _buffer += sizeof(ctrlLength);
 
-  // Read the control address.
+  // Unpack the zeromq control address.
   this->ctrl = std::string(_buffer, _buffer + ctrlLength);
   _buffer += ctrlLength;
 
-  // Read the node UUID length.
+  // Unpack the node UUID length.
   size_t nUuidLength;
   memcpy(&nUuidLength, _buffer, sizeof(nUuidLength));
   _buffer += sizeof(nUuidLength);
 
-  // Read the node UUID.
+  // Unpack the node UUID.
   this->nUuid = std::string(_buffer, _buffer + nUuidLength);
   _buffer += nUuidLength;
 
-  // Read the topic scope.
+  // Unpack the topic scope.
   memcpy(&this->scope, _buffer, sizeof(this->scope));
 
   return sizeof(topicLength) + topicLength +
@@ -454,6 +476,7 @@ size_t AdvertiseMsg::GetMsgLength()
 //////////////////////////////////////////////////
 size_t AdvertiseMsg::Pack(char *_buffer)
 {
+  // Pack the common part of any advertise message.
   size_t len = AdvertiseBase::Pack(_buffer);
   if (len == 0)
     return 0;
@@ -467,9 +490,12 @@ size_t AdvertiseMsg::Pack(char *_buffer)
 
   _buffer += len;
 
+  // Pack the length of the probouf name contained in the message.
   size_t msgTypeNameLength = this->msgTypeName.size();
   memcpy(_buffer, &msgTypeNameLength, sizeof(msgTypeNameLength));
   _buffer += sizeof(msgTypeNameLength);
+
+  // Pack the protobuf name contained in the message.
   memcpy(_buffer, this->msgTypeName.data(), msgTypeNameLength);
 
   return this->GetMsgLength();
@@ -478,14 +504,15 @@ size_t AdvertiseMsg::Pack(char *_buffer)
 //////////////////////////////////////////////////
 size_t AdvertiseMsg::UnpackBody(char *_buffer)
 {
+  // Unpack the common part of any advertise message.
   _buffer += AdvertiseBase::UnpackBody(_buffer);
 
-  // Read the msgTypeName length.
+  // Unpack the msgTypeName length.
   size_t msgTypeNameLen;
   memcpy(&msgTypeNameLen, _buffer, sizeof(msgTypeNameLen));
   _buffer += sizeof(msgTypeNameLen);
 
-  // Read the msgTypeName.
+  // Unpack the msgTypeName.
   this->msgTypeName = std::string(_buffer, _buffer + msgTypeNameLen);
 
   return this->GetMsgLength() - this->GetHeader().GetHeaderLength();
@@ -541,6 +568,7 @@ size_t AdvertiseSrv::GetMsgLength()
 //////////////////////////////////////////////////
 size_t AdvertiseSrv::Pack(char *_buffer)
 {
+  // Pack the common part of any advertise message.
   size_t len = AdvertiseBase::Pack(_buffer);
   if (len == 0)
     return 0;
@@ -554,14 +582,21 @@ size_t AdvertiseSrv::Pack(char *_buffer)
 
   _buffer += len;
 
+  // Pack the length of the protobuf name used as a service call request.
   size_t reqTypeNameLength = this->reqTypeName.size();
   memcpy(_buffer, &reqTypeNameLength, sizeof(reqTypeNameLength));
   _buffer += sizeof(reqTypeNameLength);
+
+  // Pack the protobuf name used as a service call request.
   memcpy(_buffer, this->reqTypeName.data(), reqTypeNameLength);
   _buffer += reqTypeNameLength;
+
+  // Pack the length of the protobuf name used as a service call response.
   size_t repTypeNameLength = this->repTypeName.size();
   memcpy(_buffer, &repTypeNameLength, sizeof(repTypeNameLength));
   _buffer += sizeof(repTypeNameLength);
+
+  // Pack the protobuf name used as a service call response.
   memcpy(_buffer, this->repTypeName.data(), repTypeNameLength);
 
   return this->GetMsgLength();
@@ -570,23 +605,24 @@ size_t AdvertiseSrv::Pack(char *_buffer)
 //////////////////////////////////////////////////
 size_t AdvertiseSrv::UnpackBody(char *_buffer)
 {
+  // Unpack the common part of any advertise message.
   _buffer += AdvertiseBase::UnpackBody(_buffer);
 
-  // Read the reqTypeName length.
+  // Unpack the reqTypeName length.
   size_t reqTypeNameLen;
   memcpy(&reqTypeNameLen, _buffer, sizeof(reqTypeNameLen));
   _buffer += sizeof(reqTypeNameLen);
 
-  // Read the reqTypeName.
+  // Unpack the reqTypeName.
   this->reqTypeName = std::string(_buffer, _buffer + reqTypeNameLen);
   _buffer += reqTypeNameLen;
 
-  // Read the repTypeName length.
+  // Unpack the repTypeName length.
   size_t repTypeNameLen;
   memcpy(&repTypeNameLen, _buffer, sizeof(repTypeNameLen));
   _buffer += sizeof(repTypeNameLen);
 
-  // Read the repTypeName.
+  // Unpack the repTypeName.
   this->repTypeName = std::string(_buffer, _buffer + repTypeNameLen);
 
   return this->GetMsgLength() - this->GetHeader().GetHeaderLength();
