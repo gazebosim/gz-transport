609a04f310045587d5d428dce8a1db156a99d9b7
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-08-18 22:48:23 +0200

    Define protobuf files as GENERATED

--- cmake/TestUtils.cmake
+++ cmake/TestUtils.cmake
@@ -7,6 +7,9 @@ macro (ign_build_tests)
     if(USE_LOW_MEMORY_TESTS)
       add_definitions(-DUSE_LOW_MEMORY_TESTS=1)
     endif(USE_LOW_MEMORY_TESTS)
+
+    set_source_files_properties(${PROTO_SRC} PROPERTIES GENERATED TRUE)
+    
     add_executable(${BINARY_NAME} ${GTEST_SOURCE_file} ${PROTO_SRC})
 
     add_dependencies(${BINARY_NAME}
