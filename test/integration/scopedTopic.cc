3f967722dab712e2dc2412477baa6161462da5ef
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-10-01 19:07:16 +0200

    Port test from using fork to make external binary calls

--- test/integration/scopedTopic.cc
+++ test/integration/scopedTopic.cc
@@ -20,6 +20,11 @@
 #include "ignition/transport/Node.hh"
 #include "gtest/gtest.h"
 #include "msg/int.pb.h"
+#include "test_config.h"
+
+#ifdef _WIN32
+  #include <filesystem>
+#endif
 
 using namespace ignition;
 
@@ -29,61 +34,31 @@ std::string topic = "/foo";
 int data = 5;
 
 //////////////////////////////////////////////////
-/// \brief Function is called everytime a topic update is received.
-void cb(const std::string &_topic, const transport::msgs::Int &_msg)
-{
-  EXPECT_EQ(_topic, topic);
-  EXPECT_EQ(_msg.data(), data);
-  cbExecuted = true;
-}
-
-//////////////////////////////////////////////////
-void runSubscriber()
-{
-  cbExecuted = false;
-  transport::Node node;
-
-  EXPECT_TRUE(node.Subscribe(topic, cb));
-
-  int i = 0;
-  while (i < 100 && !cbExecuted)
-  {
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    ++i;
-  }
-
-  // Check that the message was not received because the scope was Process.
-  EXPECT_FALSE(cbExecuted);
-  cbExecuted = false;
-}
-
-//////////////////////////////////////////////////
 /// \brief Two different nodes, each one running in a different process. The
 /// publisher advertises the topic as "process". This test checks that the topic
 /// is not seen by the other node running in a different process.
 TEST(ScopedTopicTest, ProcessTest)
 {
-  pid_t pid = fork();
 
-  if (pid == 0)
-    runSubscriber();
-  else
-  {
-    transport::msgs::Int msg;
-    msg.set_data(data);
+#ifdef _WIN32
+  std::path subscriber_path = PROJECT_BINARY_PATH;
+  subscriber_path /= "test/integration/INTEGRATION_subscriber_app";
+#else
+  std::string subscriber_path = PROJECT_BINARY_PATH;
+  subscriber_path += "/test/integration/INTEGRATION_subscriber_app";
+#endif
 
-    transport::Node node1;
+  ASSERT_EQ(system(subscriber_path.c_str()),0) << "Failed to run system call";
+  transport::msgs::Int msg;
+  msg.set_data(data);
 
-    EXPECT_TRUE(node1.Advertise(topic, transport::Scope::Process));
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    EXPECT_TRUE(node1.Publish(topic, msg));
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    EXPECT_TRUE(node1.Publish(topic, msg));
+  transport::Node node1;
 
-    // Wait for the child process to return.
-    int status;
-    waitpid(pid, &status, 0);
-  }
+  EXPECT_TRUE(node1.Advertise(topic, transport::Scope::Process));
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  EXPECT_TRUE(node1.Publish(topic, msg));
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  EXPECT_TRUE(node1.Publish(topic, msg));
 }
 
 //////////////////////////////////////////////////
