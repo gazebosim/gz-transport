03dadf37f8922f6a23df0c3a16d7e39a8e8f73a3
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-10-02 18:59:32 +0200

    Use standard c++

--- test/integration/twoProcessesPubSubSubscriber_aux.cc
+++ test/integration/twoProcessesPubSubSubscriber_aux.cc
@@ -75,7 +75,7 @@ void runSubscriber()
   
   int interval = 100;
   
-  while (!cbExecuted or !cb2Executed) {
+  while (!cbExecuted || !cb2Executed) {
     std::cout << "Waiting ... " << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     interval--;
