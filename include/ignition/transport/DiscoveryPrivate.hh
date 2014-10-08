1d2a947500d89026a1c8924edaa2c5d01d31dca8
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-10-08 16:46:27 +0200

    Initial port to support Windows
    TODO: tests are not working on WIN32

--- include/ignition/transport/DiscoveryPrivate.hh
+++ include/ignition/transport/DiscoveryPrivate.hh
@@ -22,8 +22,8 @@
 #include <map>
 #include <memory>
 #include <mutex>
-#include <string>
 #include <thread>
+#include <string>
 #include <vector>
 #include "ignition/transport/Helpers.hh"
 #include "ignition/transport/Packet.hh"
@@ -41,7 +41,7 @@ namespace ignition
     {
       /// \def Timestamp
       /// \brief Used to evaluate the validity of a discovery entry.
-      typedef std::chrono::time_point<std::chrono::steady_clock> Timestamp;
+      typedef std::chrono::steady_clock::time_point Timestamp;
 
       /// \brief Constructor.
       /// \param[in] _pUuid This discovery instance will run inside a
