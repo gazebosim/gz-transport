64f6c658dc5ceb8f299a1c39952fa67e28566028
gerkey <gerkey@osrfoundation.org>
2014-10-21 12:28:50 -0700

    switch to Release

--- configure.bat
+++ configure.bat
@@ -1,7 +1,6 @@
 @rem Run CMake, pointing to sibling directories containing dependencies.
 @rem Note that zmq and cppzmq are relative to the source dir, while 
 @rem protobuf is relative to the build dir.  Not sure why.
-@rem cmake -DZeroMQ_ROOT_DIR="..\ZeroMQ 3.2.4" -DPROTOBUF_SRC_ROOT_FOLDER="..\..\protobuf-2.6.0-win32-vc12" -DCPPZMQ_HEADER_PATH="..\cppzmq" -DCMAKE_INSTALL_PREFIX="install" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release .. 
-cmake -DZeroMQ_ROOT_DIR="..\ZeroMQ 3.2.4" -DPROTOBUF_SRC_ROOT_FOLDER="..\..\protobuf-2.6.0-win32-vc12" -DCPPZMQ_HEADER_PATH="..\cppzmq" -DCMAKE_INSTALL_PREFIX="install" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Debug .. 
+cmake -DZeroMQ_ROOT_DIR="..\ZeroMQ 3.2.4" -DPROTOBUF_SRC_ROOT_FOLDER="..\..\protobuf-2.6.0-win32-vc12" -DCPPZMQ_HEADER_PATH="..\cppzmq" -DCMAKE_INSTALL_PREFIX="install" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release .. 
 @if %errorlevel% neq 0 exit /b %errorlevel%
 @echo Configuration complete.  To build, run `nmake`
