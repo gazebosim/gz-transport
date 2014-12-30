af59c4f2801b800623dfd5b6d08aba665891309a
Carlos Agüero <caguero@osrfoundation.org>
2014-12-30 13:50:39 +0100

    Tweaking test.

--- test/integration/scopedTopic.cc
+++ test/integration/scopedTopic.cc
@@ -49,7 +49,7 @@ TEST(ScopedTopicTest, ProcessTest)
 
   transport::Node node1(partition, ns);
 
-  EXPECT_TRUE(node1.Advertise(topic, transport::Scope::Process));
+  EXPECT_TRUE(node1.Advertise(topic, transport::Scope_t::Process));
   std::this_thread::sleep_for(std::chrono::milliseconds(500));
   EXPECT_TRUE(node1.Publish(topic, msg));
   std::this_thread::sleep_for(std::chrono::milliseconds(500));
