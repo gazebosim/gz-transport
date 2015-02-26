d4c9fa268343aac8ded7536b21fee9b55cc32a3d
Carlos Agüero <caguero@osrfoundation.org>
2015-02-26 15:41:08 -0800

    Remove debug info.

--- src/Discovery.cc
+++ src/Discovery.cc
@@ -83,7 +83,6 @@ Discovery::Discovery(const std::string &_pUuid, bool _verbose)
 
   // Get this host IP address.
   this->dataPtr->hostAddr = determineHost();
-  std::cout << "Using: " << this->dataPtr->hostAddr << std::endl;
 
   // Get the list of network interfaces in this host.
   this->dataPtr->hostInterfaces = determineInterfaces();
