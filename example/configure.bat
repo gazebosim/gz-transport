81cf6e2616dc2711ab0e766f47ed55c039cbf4b8
Carlos Aguero <caguero@osrfoundation.org>
2015-03-10 15:34:41 -0700

    Fix example compilation.

--- example/configure.bat
+++ example/configure.bat
@@ -6,6 +6,6 @@
 @set build_bitness=32
 @if not "%2"=="" set build_bitness=%2
 @echo Configuring for build type %build_type%
-cmake -DZeroMQ_ROOT_DIR="..\..\ZeroMQ 3.2.4" -DPROTOBUF_SRC_ROOT_FOLDER="..\..\..\protobuf-2.6.0-win%build_bitness%-vc12" -DCPPZMQ_HEADER_PATH="..\..\cppzmq" -DCMAKE_INSTALL_PREFIX="install" -DCMAKE_PREFIX_PATH="..\build\install\%build_type%" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=%build_type% .. 
+cmake -DZeroMQ_ROOT_DIR="..\..\ZeroMQ 3.2.4" -DPROTOBUF_SRC_ROOT_FOLDER="..\..\..\protobuf-2.6.0-win%build_bitness%-vc12" -DCPPZMQ_HEADER_PATH="..\..\cppzmq" -DCMAKE_INSTALL_PREFIX="install" -DCMAKE_PREFIX_PATH="..\build\install\%build_type%\lib\cmake\ignition-transport" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=%build_type% .. 
 @if %errorlevel% neq 0 exit /b %errorlevel%
 @echo Configuration complete.  To build, run `nmake`
