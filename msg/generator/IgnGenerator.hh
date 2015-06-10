83698b12ce5775e926e2bfbdf760d6238199f5c6
Nate Koenig <natekoenig@gmail.com>
2015-06-10 10:38:55 -0700

    _HH_

--- msg/generator/IgnGenerator.hh
+++ msg/generator/IgnGenerator.hh
@@ -14,8 +14,8 @@
  * limitations under the License.
  *
 */
-#ifndef _IGN_TRANSPORT_GENERATOR_H_
-#define _IGN_TRANSPORT_GENERATOR_H_
+#ifndef _IGN_TRANSPORT_GENERATOR_HH_
+#define _IGN_TRANSPORT_GENERATOR_HH_
 
 #include <google/protobuf/compiler/code_generator.h>
 #include <string>
