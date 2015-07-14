17a9ed72718bfff495e3c2794f003f2caf79598a
caguero <devnull@localhost>
2015-07-13 17:19:15 -0700

    Multiple cmake tweaks for Windows.

--- example/configure.bat
+++ example/configure.bat
@@ -3,15 +3,21 @@
 @rem protobuf is relative to the build dir.  Not sure why.
 @set build_type=Release
 @if not "%1"=="" set build_type=%1
-@set build_bitness=32
+@set build_bitness=64
 @if not "%2"=="" set build_bitness=%2
 @echo Configuring for build type %build_type%
-cmake -DZeroMQ_ROOT_DIR="..\..\ZeroMQ 3.2.4"^
-      -DPROTOBUF_SRC_ROOT_FOLDER="..\..\..\protobuf-2.6.0-win%build_bitness%-vc12"^
-      -DCPPZMQ_HEADER_PATH="..\..\cppzmq"^
+
+@set PROTOBUF_PATH=%cd%\..\..\..\protobuf-2.6.0-win%build_bitness%-vc12
+@set ZEROMQ_PATH=%cd%\..\..\..\ZeroMQ 3.2.4
+@set CPPZMQ_PATH=%cd%\..\..\..\cppzmq
+@set IGN_TRANSPORT_PATH=%cd%\..\..\build\install\%build_type%
+
+cmake -G "NMake Makefiles"^
+      -DZeroMQ_ROOT_DIR="%ZEROMQ_PATH%"^
+      -DPROTOBUF_SRC_ROOT_FOLDER="%PROTOBUF_PATH%"^
+      -DCPPZMQ_HEADER_PATH="%CPPZMQ_PATH%"^
       -DCMAKE_INSTALL_PREFIX="install"^
-      -DCMAKE_PREFIX_PATH="..\build\install\%build_type%\lib\cmake\ignition-transport"^
-      -G "NMake Makefiles"^
+      -DCMAKE_PREFIX_PATH="%IGN_TRANSPORT_PATH%\lib\cmake\ignition-transport"^
       -DCMAKE_BUILD_TYPE=%build_type%^
       ..
 @if %errorlevel% neq 0 exit /b %errorlevel%
