d2bf71cd0909e19cdbd117fd7ee28a0c028169d3
Carlos Agüero <caguero@osrfoundation.org>
2015-09-04 17:47:07 -0700

    Fixing problems on Windows.

--- example/configure.bat
+++ example/configure.bat
@@ -3,14 +3,14 @@
 @rem protobuf is relative to the build dir.  Not sure why.
 @set build_type=Release
 @if not "%1"=="" set build_type=%1
-@set build_bitness=32
+@set build_bitness=64
 @if not "%2"=="" set build_bitness=%2
 @echo Configuring for build type %build_type%
 cmake -DZeroMQ_ROOT_DIR="..\..\ZeroMQ 3.2.4"^
       -DPROTOBUF_SRC_ROOT_FOLDER="..\..\..\protobuf-2.6.0-win%build_bitness%-vc12"^
       -DCPPZMQ_HEADER_PATH="..\..\cppzmq"^
       -DCMAKE_INSTALL_PREFIX="install"^
-      -DCMAKE_PREFIX_PATH="..\build\install\%build_type%\lib\cmake\ignition-transport"^
+      -DCMAKE_PREFIX_PATH="..\build\install\%build_type%\lib\cmake\ignition-transport0"^
       -G "NMake Makefiles"^
       -DCMAKE_BUILD_TYPE=%build_type%^
       ..
