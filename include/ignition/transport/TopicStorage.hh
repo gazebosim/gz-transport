48e0d5826861cb5a523b781a98a72e97cf6921b0
Carlos Agüero <caguero@osrfoundation.org>
2015-09-17 12:41:49 -0700

    Fix a few handlerStorage bugs.

--- include/ignition/transport/TopicStorage.hh
+++ include/ignition/transport/TopicStorage.hh
@@ -175,6 +175,7 @@ namespace ignition
         return true;
       }
 
+      /// ToDO: Directly do counter = remove(...)
       /// \brief Remove a publisher associated to a given topic and UUID pair.
       /// \param[in] _topic Topic name
       /// \param[in] _pUuid Process UUID of the publisher.
@@ -217,6 +218,7 @@ namespace ignition
         return counter > 0;
       }
 
+      /// ToDo: Replace counter = with counter +=?
       /// \brief Remove all the publishers associated to a given process.
       /// \param[in] _pUuid Process' UUID of the publisher.
       /// \return True when at least one address was removed or false otherwise.
