ee90417058be9857ec5890d756980aa5fdf8d8a7
Carlos Agüero <caguero@osrfoundation.org>
2015-09-25 18:31:38 -0700

    Restoring bitness to 32 for now.

--- configure.bat
+++ configure.bat
@@ -5,7 +5,7 @@
 @set build_type=Release
 @if not "%1"=="" set build_type=%1
 
-@set build_bitness=64
+@set build_bitness=32
 @if not "%2"=="" set build_bitness=%2
 
 @echo Configuring for build type %build_type% for %build_bitness% bits
--- example/configure.bat
+++ example/configure.bat
@@ -3,7 +3,7 @@
 @rem protobuf is relative to the build dir.  Not sure why.
 @set build_type=Release
 @if not "%1"=="" set build_type=%1
-@set build_bitness=64
+@set build_bitness=32
 @if not "%2"=="" set build_bitness=%2
 @echo Configuring for build type %build_type%
 cmake -DZeroMQ_ROOT_DIR="..\..\ZeroMQ 3.2.4"^
