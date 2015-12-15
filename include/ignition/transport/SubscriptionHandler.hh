cdcf9f933e244f31723b533ef593a4eb3bd424ec
Carlos Agüero <caguero@osrfoundation.org>
2015-12-14 17:04:27 -0800

    Add doxygen.

--- include/ignition/transport/SubscriptionHandler.hh
+++ include/ignition/transport/SubscriptionHandler.hh
@@ -69,6 +69,7 @@ namespace ignition
 
       /// \brief Get the type of the messages from which this subscriber
       /// handler is subscribed.
+      /// \return String representation of the message type.
       public: virtual std::string GetTypeName() = 0;
 
       /// \brief Get the node UUID.
