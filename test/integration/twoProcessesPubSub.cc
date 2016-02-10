6f0019663fc7973936b708e9e26e6fe153214658
Carlos Agüero <caguero@osrfoundation.org>
2016-02-10 14:04:42 -0800

    Restore sleep in test

--- test/integration/twoProcessesPubSub.cc
+++ test/integration/twoProcessesPubSub.cc
@@ -164,6 +164,9 @@ TEST(twoProcPubSub, TopicList)
 
   reset();
 
+  // We need some time for discovering the other node.
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
   transport::Node node;
   std::vector<std::string> topics;
 
