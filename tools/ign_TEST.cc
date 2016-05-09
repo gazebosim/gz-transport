2ab288b274ed2de5704fd514cb579d80b4679239
Nate Koenig <natekoenig@gmail.com>
2016-05-09 07:35:40 -0700

    Added a test

--- tools/ign_TEST.cc
+++ tools/ign_TEST.cc
@@ -25,7 +25,8 @@
 
 using namespace ignition;
 
-std::string partition;
+std::string g_partition;
+std::string g_topicCBStr;
 
 /////////////////////////////////////////////////
 std::string custom_exec_str(std::string _cmd)
@@ -59,6 +60,13 @@ void srvEcho(const ignition::msgs::Int32 &_req, ignition::msgs::Int32 &_rep,
 }
 
 //////////////////////////////////////////////////
+/// \brief Topic callback
+void topicCB(const ignition::msgs::StringMsg &_msg)
+{
+  g_topicCBStr = _msg.data();
+}
+
+//////////////////////////////////////////////////
 /// \brief Check 'ign topic -l' running the advertiser on a different process.
 TEST(ignTest, TopicList)
 {
@@ -68,7 +76,7 @@ TEST(ignTest, TopicList)
     "test/integration/INTEGRATION_twoProcessesPublisher_aux");
 
   testing::forkHandlerType pi = testing::forkAndRun(publisher_path.c_str(),
-    partition.c_str());
+    g_partition.c_str());
 
   // Check the 'ign topic -l' command.
   std::string ign = std::string(IGN_PATH) + "/ign";
@@ -89,7 +97,7 @@ TEST(ignTest, TopicInfo)
     "test/integration/INTEGRATION_twoProcessesPublisher_aux");
 
   testing::forkHandlerType pi = testing::forkAndRun(publisher_path.c_str(),
-    partition.c_str());
+    g_partition.c_str());
 
   // Check the 'ign topic -i' command.
   std::string ign = std::string(IGN_PATH) + "/ign";
@@ -113,7 +121,7 @@ TEST(ignTest, ServiceList)
     "test/integration/INTEGRATION_twoProcessesSrvCallReplier_aux");
 
   testing::forkHandlerType pi = testing::forkAndRun(replier_path.c_str(),
-    partition.c_str());
+    g_partition.c_str());
 
   // Check the 'ign service -l' command.
   std::string ign = std::string(IGN_PATH) + "/ign";
@@ -134,7 +142,7 @@ TEST(ignTest, ServiceInfo)
     "test/integration/INTEGRATION_twoProcessesSrvCallReplier_aux");
 
   testing::forkHandlerType pi = testing::forkAndRun(replier_path.c_str(),
-    partition.c_str());
+    g_partition.c_str());
 
   // Check the 'ign service -i' command.
   std::string ign = std::string(IGN_PATH) + "/ign";
@@ -217,15 +225,35 @@ TEST(ignTest, ServiceInfoSameProc)
   EXPECT_TRUE(output.find("ignition.msgs.Int32") != std::string::npos);
 }
 
+
+//////////////////////////////////////////////////
+/// \brief Check 'ign service -p' to send a message.
+TEST(ignTest, PublicMsg)
+{
+  ignition::transport::Node node;
+  g_topicCBStr = "bad_value";
+  EXPECT_TRUE(node.Subscribe("/bar", topicCB));
+
+  // Check the 'ign service -p' command.
+  std::string ign = std::string(IGN_PATH) + "/ign";
+  std::string output = custom_exec_str(ign +
+      " topic -t /bar -m ign_msgs.StringMsg -p 'data:\"good_value\"'");
+
+  ASSERT_TRUE(output.empty());
+  std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+  EXPECT_EQ(g_topicCBStr, "good_value");
+}
+
 /////////////////////////////////////////////////
 /// Main
 int main(int argc, char **argv)
 {
   // Get a random partition name.
-  partition = testing::getRandomNumber();
+  g_partition = testing::getRandomNumber();
 
   // Set the partition name for this process.
-  setenv("IGN_PARTITION", partition.c_str(), 1);
+  setenv("IGN_PARTITION", g_partition.c_str(), 1);
 
   // Set IGN_CONFIG_PATH to the directory where the .yaml configuration files
   // is located.
