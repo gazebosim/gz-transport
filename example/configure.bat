7ba66e260837532acf57c3fa91df6f93f5452984
nkoenig <nkoenig@osrfoundation.org>
2016-06-28 12:16:29 -0700

    Backport cmake fix

--- example/configure.bat
+++ example/configure.bat
@@ -8,16 +8,12 @@
 @echo Configuring for build type %build_type%
 
 @set PROTOBUF_PATH=%cd%\..\..\..\protobuf-2.6.0-win%build_bitness%-vc12
-@set ZEROMQ_PATH=%cd%\..\..\..\ZeroMQ 3.2.4
-@set CPPZMQ_PATH=%cd%\..\..\..\cppzmq
 @set IGN_TRANSPORT_PATH=%cd%\..\..\build\install\%build_type%
 
 cmake -G "NMake Makefiles"^
-      -DZeroMQ_ROOT_DIR="%ZEROMQ_PATH%"^
-      -DPROTOBUF_SRC_ROOT_FOLDER="%PROTOBUF_PATH%"^
-      -DCPPZMQ_HEADER_PATH="%CPPZMQ_PATH%"^
-      -DCMAKE_INSTALL_PREFIX="install"^
       -DCMAKE_PREFIX_PATH="%IGN_TRANSPORT_PATH%\lib\cmake\ignition-transport1"^
+      -DPROTOBUF_SRC_ROOT_FOLDER="%PROTOBUF_PATH%"^
+      -DCMAKE_INSTALL_PREFIX="install"^
       -DCMAKE_BUILD_TYPE=%build_type%^
       ..
 
