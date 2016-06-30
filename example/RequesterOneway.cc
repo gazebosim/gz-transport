5f71dd7b34d4647ffa90a3dc5b2e0fc38495db59
Amitoj <iamamitoj@gmail.com.com>
2016-06-30 23:55:33 +0530

    update example/RequesterOneway.cc

--- example/RequesterOneway.cc
+++ example/RequesterOneway.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Open Source Robotics Foundation
+ * Copyright (C) 2016 Open Source Robotics Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 
 #include <iostream>
 #include <ignition/transport.hh>
-#include "msgs/stringmsg.pb.h"
+#include <ignition/msgs.hh>
 
 //////////////////////////////////////////////////
 int main(int argc, char **argv)
