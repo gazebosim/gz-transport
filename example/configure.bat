f47e37b4217c4c6aecec635f3a327871c5600228
nkoenig <nkoenig@osrfoundation.org>
2016-06-30 14:45:06 -0700

    Remove ign-msgs from examples

--- example/configure.bat
+++ example/configure.bat
@@ -9,15 +9,12 @@
 
 @set PROTOBUF_PATH=%cd%\..\..\..\protobuf-2.6.0-win%build_bitness%-vc12
 @set IGN_TRANSPORT_PATH=%cd%\..\..\build\install\%build_type%
-@set IGNITION-MSGS_PATH=%cd%\..\..\..\ign-msgs\build\install\%build_type%
-@set IGNITION-MSGS_CMAKE_PREFIX_PATH=%IGNITION-MSGS_PATH%\CMake
 @set IGNITION-MATH_PATH=%cd%\..\..\..\ign-math\build\install\%build_type%
 @set IGNITION-MATH_CMAKE_PREFIX_PATH=%IGNITION-MATH_PATH%\CMake
 
 cmake -G "NMake Makefiles"^
-      -DCMAKE_PREFIX_PATH="%IGN_TRANSPORT_PATH%\lib\cmake\ignition-transport1;%IGNITION-MSGS_CMAKE_PREFIX_PATH%;%IGNITION-MATH_CMAKE_PREFIX_PATH%;"^
+      -DCMAKE_PREFIX_PATH="%IGN_TRANSPORT_PATH%\lib\cmake\ignition-transport1;%IGNITION-MATH_CMAKE_PREFIX_PATH%;"^
       -DPROTOBUF_SRC_ROOT_FOLDER="%PROTOBUF_PATH%"^
-      -DIGNITION-MSGS_FOLDER="%IGNITION-MSGS_PATH%"^
       -DCMAKE_INSTALL_PREFIX="install"^
       -DCMAKE_BUILD_TYPE=%build_type%^
       ..
