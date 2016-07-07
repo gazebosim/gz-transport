9f4e09d84226d74759b61aaf14b93bd073f56fb3
Amitoj <iamamitoj@gmail.com.com>
2016-07-07 23:10:07 +0530

    add integration tests files

---
+++ test/integration/twoProcessesSrvCallWithoutOutputReplierIncreasing_aux.cc
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2016 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <chrono>
+#include <climits>
+#include <string>
+#include <ignition/msgs.hh>
+
+#include "ignition/transport/Node.hh"
+#include "gtest/gtest.h"
+#include "ignition/transport/test_config.h"
+
+using namespace ignition;
+
+static std::string g_topic = "/foo";
+static int g_data = 5;
+static int kForever = INT_MAX;
+
+//////////////////////////////////////////////////
+/// \brief Provide a service without output.
+void srvWithoutOutput(const ignition::msgs::Int32 &_req)
+{
+  EXPECT_EQ(_req.data(), g_data);
+}
+
+//////////////////////////////////////////////////
+void runReplier()
+{
+  transport::Node node;
+  EXPECT_TRUE(node.Advertise(g_topic, srvWithoutOutput));
+
+  // Run the node forever. Should be killed by the test that uses this.
+  std::this_thread::sleep_for(std::chrono::milliseconds(kForever));
+}
+
+//////////////////////////////////////////////////
+TEST(twoProcSrvCallWithoutOuputReplierAux, SrvProcReplier)
+{
+  runReplier();
+}
+
+//////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
+    std::cerr << "Partition name has not be passed as argument" << std::endl;
+    return -1;
+  }
+
+  // Set the partition name for this test.
+  setenv("IGN_PARTITION", argv[1], 1);
+
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
