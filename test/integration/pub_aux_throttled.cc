a6be3818f4558c3214a803af0f8113d74607dfd0
Karishma <karishmasharma558@gmail.com>
2016-12-30 16:57:23 +0530

    Changes in test approach

---
+++ test/integration/pub_aux_throttled.cc
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2016 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#include <chrono>
+#include <string>
+#include <thread>
+#include <ignition/msgs.hh>
+
+#include "gtest/gtest.h"
+#include "ignition/transport/Node.hh"
+#include "ignition/transport/test_config.h"
+
+using namespace ignition;
+
+static std::string g_topic = "/foo";
+
+//////////////////////////////////////////////////
+/// \brief A publisher node.
+void advertiseAndPublish()
+{
+  ignition::msgs::Int32 msg;
+  msg.set_data(1.0);
+
+  transport::Node node;
+  ignition::transport::AdvertiseMessageOptions opts;
+  opts.SetMsgsPerSec(1u);
+
+  auto pub = node.Advertise<ignition::msgs::Int32>(g_topic, opts);
+  std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+  for (auto i = 0; i < 25; ++i)
+  {
+    EXPECT_TRUE(pub.Publish(msg));
+
+    // Rate: 10 msgs/sec.
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+}
+
+//////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+  {
+    std::cerr << "Partition name has not be passed as argument" << std::endl;
+    return -1;
+  }
+
+  // Set the partition name for this test.
+  setenv("IGN_PARTITION", argv[1], 1);
+
+  advertiseAndPublish();
+}
--- test/integration/twoProcessesPubSub.cc
+++ test/integration/twoProcessesPubSub.cc
@@ -213,36 +213,24 @@ TEST(NodeTest, SubThrottled)
 /// processes. The publisher publishes at a throttled frequency.
 TEST(NodeTest, PubThrottled)
 {
-  std::string subscriberPath = testing::portablePathUnion(
+  std::string publisherPath = testing::portablePathUnion(
      PROJECT_BINARY_PATH,
-     "test/integration/INTEGRATION_sub_aux");
+     "test/integration/INTEGRATION_pub_aux_throttled");
 
-  testing::forkHandlerType pi = testing::forkAndRun(subscriberPath.c_str(),
+  testing::forkHandlerType pi = testing::forkAndRun(publisherPath.c_str(),
     partition.c_str());
 
   reset();
 
   std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-  ignition::msgs::Int32 msg;
-  msg.set_data(1.0);
   transport::Node node;
-  ignition::transport::AdvertiseMessageOptions opts;
-  opts.SetMsgsPerSec(1u);
   EXPECT_TRUE(node.Subscribe(g_topic, cb));
 
-  auto pub = node.Advertise<ignition::msgs::Int32>(g_topic, opts);
-  EXPECT_TRUE(pub);
+  // Wait some time before publishing.
+  std::this_thread::sleep_for(std::chrono::milliseconds(2500));
 
-  for (auto i = 0; i < 15; ++i)
-  {
-    EXPECT_TRUE(pub.Publish(msg));
-
-    // Rate: 10 msgs/sec.
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  }
-
-  // Node published 15 messages in ~1.5 sec. We should only receive 2 messages.
+  // Node published 25 messages in ~2.5 sec. We should only receive 2 messages.
   EXPECT_EQ(counter, 2);
 
   reset();
