c33144f555b3f1dc67a724497417c68077805083
Shane Loretz <sloretz@osrfoundation.org>
2017-12-07 09:28:47 -0800

    Add license

--- tools/record/schema/schema_0.1.0.sql
+++ tools/record/schema/schema_0.1.0.sql
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2017 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
 /* Note: Use PRAGMA foreign_keys = ON; prior to writing to a database using this schema */
 
 /* Describes the schema version used in this database */
@@ -61,3 +78,4 @@ CREATE TABLE messages(
 
 /* Lots of queries are done by time received, so add an index to speed it up */
 CREATE INDEX idx_time_recv_utc ON messages (time_recv_utc);
+
