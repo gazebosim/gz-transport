9606334e87ed19e6de7da09865d432a28e251ebb
Steve Peters <scpeters@openrobotics.org>
2018-01-11 10:23:56 -0800

    use default branch of ign-cmake

--- configure.bat
+++ configure.bat
@@ -5,7 +5,7 @@
 
 :: Install dependencies
 call %win_lib% :download_unzip_install libzmq-4.2.3_cppzmq-4.2.2_vc15-x64-dll-MD.zip
-call %win_lib% :install_ign_project ign-msgs win_ci_no_build_testing
+call %win_lib% :install_ign_project ign-msgs ign-msgs1
 
 :: Set configuration variables
 @set build_type=Release
