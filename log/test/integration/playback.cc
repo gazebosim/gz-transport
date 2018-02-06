d7fc137bb745e5b95af00a5a293b40d18e058250
Shane Loretz <sloretz@osrfoundation.org>
2018-02-06 13:10:50 -0800

    Pull playback and record integration tests from command_line_log

---
+++ log/test/integration/playback.cc
@@ -0,0 +1,302 @@
+/*
+ * Copyright (C) 2018 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gtest/gtest.h>
+
+#include <ignition/common/Filesystem.hh>
+
+#include <ignition/transport/Node.hh>
+#include <ignition/transport/log/Recorder.hh>
+#include <ignition/transport/log/Playback.hh>
+#include <ignition/transport/log/Log.hh>
+
+#include "ChirpParams.hh"
+
+struct MessageInformation
+{
+  public: std::string data;
+  public: std::string type;
+  public: std::string topic;
+};
+
+static std::mutex dataMutex;
+
+/// \brief This is used within lambda callbacks to keep track of incoming
+/// messages.
+/// \param[in] _archive A vector that will store the incoming message
+/// information. This must be passed from a lambda which has captured a vector.
+/// \param[in] _data The data passed by the SubscribeRaw
+/// \param[in] _len The length of data passed by the SubscribeRaw
+/// \param[in] _msgInfo The metainfo about the message, provided by the
+/// SubscribeRaw.
+void TrackMessages(std::vector<MessageInformation> &_archive,
+                   const char *_data,
+                   std::size_t _len,
+                   const ignition::transport::MessageInfo &_msgInfo)
+{
+  MessageInformation info;
+  info.data = std::string(_data, _len);
+  info.type = _msgInfo.Type();
+  info.topic = _msgInfo.Topic();
+
+  std::unique_lock<std::mutex> lock(dataMutex);
+  _archive.push_back(info);
+}
+
+
+/// \brief Compare sent and received messages
+/// \param[in] _recorded messages that were recorded
+/// \param[in] _played messages that were published
+void ExpectSameMessages(const std::vector<MessageInformation> &_recorded,
+    const std::vector<MessageInformation> &_played)
+{
+  for (std::size_t i = 0; i < _recorded.size(); ++i)
+  {
+    const MessageInformation &original = _recorded[i];
+    const MessageInformation &playedBack = _played[i];
+
+    EXPECT_EQ(original.data, playedBack.data);
+    EXPECT_EQ(original.type, playedBack.type);
+    EXPECT_EQ(original.topic, playedBack.topic);
+  }
+}
+
+
+/// \brief Record a log and then play it back. Verify that the playback matches
+/// the original.
+TEST(playback, ReplayLog)
+{
+  std::vector<std::string> topics = {"/foo", "/bar", "/baz"};
+
+  std::vector<MessageInformation> incomingData;
+
+  auto callback = [&incomingData](
+      const char *_data,
+      std::size_t _len,
+      const ignition::transport::MessageInfo &_msgInfo)
+  {
+    TrackMessages(incomingData, _data, _len, _msgInfo);
+  };
+
+  ignition::transport::Node node;
+  ignition::transport::log::Recorder recorder;
+
+  for (const std::string &topic : topics)
+  {
+    node.SubscribeRaw(topic, callback);
+    recorder.AddTopic(topic);
+  }
+
+  const std::string logName = IGN_TRANSPORT_LOG_BUILD_PATH"/test.log";
+  ignition::common::removeFile(logName);
+  EXPECT_EQ(ignition::transport::log::RecorderError::NO_ERROR,
+    recorder.Start(logName));
+
+  const int numChirps = 100;
+  testing::forkHandlerType chirper =
+    ignition::transport::log::test::BeginChirps(topics, numChirps);
+
+  // Wait for the chirping to finish
+  testing::waitAndCleanupFork(chirper);
+
+  // Wait to make sure our callbacks are done processing the incoming messages
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  // Stop recording so we can safely play back the log
+  recorder.Stop();
+
+  // Make a copy of the data so we can compare it later
+  std::vector<MessageInformation> originalData = incomingData;
+
+  // Clear out the old data so we can recreate it during the playback
+  incomingData.clear();
+
+  ignition::transport::log::Playback playback(logName);
+
+  for (const std::string &topic : topics)
+  {
+    playback.AddTopic(topic);
+  }
+
+  playback.Start();
+
+  std::cout << "Waiting to for playback to finish..." << std::endl;
+  playback.WaitUntilFinished();
+  // Wait to make sure our callbacks are done processing the incoming messages
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::cout << "Playback finished!" << std::endl;
+
+  playback.Stop();
+
+  ExpectSameMessages(originalData, incomingData);
+}
+
+
+TEST(playback, ReplayNoSuchTopic)
+{
+  ignition::transport::log::Recorder recorder;
+  const std::string logName = IGN_TRANSPORT_LOG_BUILD_PATH"/test.log";
+  ignition::common::removeFile(logName);
+  EXPECT_EQ(ignition::transport::log::RecorderError::NO_ERROR,
+    recorder.Start(logName));
+  recorder.Stop();
+
+  ignition::transport::log::Playback playback(logName);
+  EXPECT_EQ(ignition::transport::log::PlaybackError::NO_SUCH_TOPIC,
+      playback.AddTopic("/DNE"));
+  EXPECT_EQ(0, playback.AddTopic(std::regex("/DNE")));
+}
+
+
+/// \brief Record a log and then play it back. Verify that the playback matches
+/// the original.
+TEST(playback, ReplayLogRegex)
+{
+  std::vector<std::string> topics = {"/foo", "/bar", "/baz"};
+
+  std::vector<MessageInformation> incomingData;
+
+  auto callback = [&incomingData](
+      const char *_data,
+      std::size_t _len,
+      const ignition::transport::MessageInfo &_msgInfo)
+  {
+    TrackMessages(incomingData, _data, _len, _msgInfo);
+  };
+
+  ignition::transport::Node node;
+  ignition::transport::log::Recorder recorder;
+
+  for (const std::string &topic : topics)
+  {
+    node.SubscribeRaw(topic, callback);
+  }
+  recorder.AddTopic(std::regex(".*"));
+
+  const std::string logName = IGN_TRANSPORT_LOG_BUILD_PATH"/test.log";
+  ignition::common::removeFile(logName);
+  EXPECT_EQ(ignition::transport::log::RecorderError::NO_ERROR,
+    recorder.Start(logName));
+
+  const int numChirps = 100;
+  testing::forkHandlerType chirper =
+      ignition::transport::log::test::BeginChirps(topics, numChirps);
+
+  // Wait for the chirping to finish
+  testing::waitAndCleanupFork(chirper);
+
+  // Wait to make sure our callbacks are done processing the incoming messages
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  // Stop recording so we can safely play back the log
+  recorder.Stop();
+
+  // Make a copy of the data so we can compare it later
+  std::vector<MessageInformation> originalData = incomingData;
+
+  // Clear out the old data so we can recreate it during the playback
+  incomingData.clear();
+
+  ignition::transport::log::Playback playback(logName);
+  playback.Start();
+  std::cout << "Waiting to for playback to finish..." << std::endl;
+  playback.WaitUntilFinished();
+  // Wait to make sure our callbacks are done processing the incoming messages
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::cout << "Playback finished!" << std::endl;
+
+  playback.Stop();
+
+  ExpectSameMessages(originalData, incomingData);
+}
+
+/// \brief Record a log and then play it back. Verify that the playback matches
+/// the original.
+TEST(playback, ReplayLogMoveInstances)
+{
+  std::vector<std::string> topics = {"/foo", "/bar", "/baz"};
+
+  std::vector<MessageInformation> incomingData;
+
+  auto callback = [&incomingData](
+      const char *_data,
+      std::size_t _len,
+      const ignition::transport::MessageInfo &_msgInfo)
+  {
+    TrackMessages(incomingData, _data, _len, _msgInfo);
+  };
+
+  ignition::transport::Node node;
+  ignition::transport::log::Recorder recorder_orig;
+
+  for (const std::string &topic : topics)
+  {
+    node.SubscribeRaw(topic, callback);
+  }
+  recorder_orig.AddTopic(std::regex(".*"));
+
+  ignition::transport::log::Recorder recorder(std::move(recorder_orig));
+
+  const std::string logName = IGN_TRANSPORT_LOG_BUILD_PATH"/test.log";
+  ignition::common::removeFile(logName);
+  EXPECT_EQ(ignition::transport::log::RecorderError::NO_ERROR,
+    recorder.Start(logName));
+
+  const int numChirps = 100;
+  testing::forkHandlerType chirper =
+      ignition::transport::log::test::BeginChirps(topics, numChirps);
+
+  // Wait for the chirping to finish
+  testing::waitAndCleanupFork(chirper);
+
+  // Wait to make sure our callbacks are done processing the incoming messages
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  // Stop recording so we can safely play back the log
+  recorder.Stop();
+
+  // Make a copy of the data so we can compare it later
+  std::vector<MessageInformation> originalData = incomingData;
+
+  // Clear out the old data so we can recreate it during the playback
+  incomingData.clear();
+
+  ignition::transport::log::Playback playback_orig(logName);
+  playback_orig.AddTopic(std::regex(".*"));
+  ignition::transport::log::Playback playback(std::move(playback_orig));
+  playback.Start();
+
+  std::cout << "Waiting to for playback to finish..." << std::endl;
+  playback.WaitUntilFinished();
+  // Wait to make sure our callbacks are done processing the incoming messages
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::cout << "Playback finished!" << std::endl;
+
+  playback.Stop();
+
+  ExpectSameMessages(originalData, incomingData);
+}
+
+//////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+  setenv(ignition::transport::log::SchemaLocationEnvVar.c_str(),
+         IGN_TRANSPORT_LOG_SQL_PATH, 1);
+
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ log/test/integration/recorder.cc
@@ -0,0 +1,286 @@
+/*
+ * Copyright (C) 2017 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gtest/gtest.h>
+
+#include <ignition/common/Filesystem.hh>
+
+#include <ignition/transport/Node.hh>
+#include <ignition/transport/log/Recorder.hh>
+#include <ignition/transport/log/Log.hh>
+
+#include "ChirpParams.hh"
+
+//////////////////////////////////////////////////
+/// \brief VerifyMessage is intended to be used by the
+/// BeginRecordingXxxxBeforeAdvertisement tests.
+/// \param _msg The message we are currently verifying
+/// \param _msgCount The number of messages that we have iterated through so far
+/// \param _numTopics The number of topics that we are expecting messages from
+/// \param VerifyTopic A boolean function that can verify that the topic name is
+/// valid.
+/// \return True if the message we are viewing is valid.
+void VerifyMessage(const ignition::transport::log::Message &_msg,
+                   const int64_t _msgCount,
+                   const int64_t _numTopics,
+                   const std::function<bool(const std::string&)> &VerifyTopic)
+{
+  using MsgType = ignition::transport::log::test::ChirpMsgType;
+
+  const std::string &data = _msg.Data();
+  const std::string &type = _msg.Type();
+  EXPECT_FALSE(data.empty());
+  EXPECT_FALSE(type.empty());
+
+  EXPECT_TRUE(VerifyTopic(_msg.Topic()));
+
+  MsgType msg;
+
+  EXPECT_EQ(msg.GetTypeName(), type);
+
+  EXPECT_TRUE(msg.ParseFromString(data));
+
+  // The chirps will count starting from 1 (hence the +1) up to numChirps for
+  // each topic. We use integer division because it automatically rounds down,
+  // which is what we want.
+  const int64_t chirpValue = _msgCount/_numTopics + 1;
+  EXPECT_EQ(chirpValue, msg.data());
+}
+
+//////////////////////////////////////////////////
+/// \brief Begin recording a set of topics before those topics are advertised
+/// or published to.
+TEST(recorder, BeginRecordingTopicsBeforeAdvertisement)
+{
+  // Remember to include a leading slash so that the VerifyTopic lambda below
+  // will work correctly. ign-transport automatically adds a leading slash to
+  // topics that don't specify one.
+  std::vector<std::string> topics = {"/foo", "/bar"};
+
+  ignition::transport::log::Recorder recorder;
+  for (const std::string &topic : topics)
+  {
+    recorder.AddTopic(topic);
+  }
+
+  const std::string logName = IGN_TRANSPORT_LOG_BUILD_PATH"/test.log";
+  ignition::common::removeFile(logName);
+
+  EXPECT_EQ(recorder.Start(logName),
+            ignition::transport::log::RecorderError::NO_ERROR);
+
+  const int numChirps = 100;
+  testing::forkHandlerType chirper =
+      ignition::transport::log::test::BeginChirps(topics, numChirps);
+
+  // Wait for the chirping to finish
+  testing::waitAndCleanupFork(chirper);
+
+  // Wait to make sure our callbacks are done processing the incoming messages
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  // Stop recording so we can safely view the log
+  recorder.Stop();
+
+  ignition::transport::log::Log log;
+  EXPECT_TRUE(log.Open(logName));
+
+  auto VerifyTopic = [&](const std::string &_topic)
+  {
+    for (const std::string &check : topics)
+    {
+      if (check == _topic)
+      {
+        return true;
+      }
+    }
+
+    std::cout << "Unexpected topic name: " << _topic << std::endl;
+
+    return false;
+  };
+
+  int64_t count = 0;
+
+  for (const ignition::transport::log::Message &msg : log.QueryMessages())
+  {
+    VerifyMessage(msg, count,
+                  static_cast<int64_t>(topics.size()),
+                  VerifyTopic);
+    ++count;
+  }
+
+  EXPECT_EQ(numChirps*static_cast<int>(topics.size()), count);
+
+  ignition::common::removeFile(logName);
+}
+
+//////////////////////////////////////////////////
+/// \brief Begin recording a set of topics after those topics have been
+/// advertised and published to. Some of the initial messages will be missed,
+/// so we only test to see that we received the very last message.
+TEST(recorder, BeginRecordingTopicsAfterAdvertisement)
+{
+  std::vector<std::string> topics = {"/foo", "/bar"};
+
+  const std::string logName = IGN_TRANSPORT_LOG_BUILD_PATH"/test.log";
+  ignition::common::removeFile(logName);
+
+  ignition::transport::log::Recorder recorder;
+
+  const int delay_ms = ignition::transport::log::test::DelayBetweenChirps_ms;
+
+  // We want to chirp for this many seconds
+  const double secondsToChirpFor = 1.5;
+
+  // ... so this is how many chirps we should emit
+  const int numChirps = static_cast<int>(
+        std::ceil(secondsToChirpFor * 1000.0/static_cast<double>(delay_ms)));
+
+  testing::forkHandlerType chirper =
+      ignition::transport::log::test::BeginChirps(topics, numChirps);
+
+  const int waitBeforeSubscribing_ms =
+      ignition::transport::log::test::DelayBeforePublishing_ms
+      + static_cast<int>(0.1*secondsToChirpFor)*1000;
+
+  std::this_thread::sleep_for(
+        std::chrono::milliseconds(waitBeforeSubscribing_ms));
+
+  for (const std::string &topic : topics)
+  {
+    recorder.AddTopic(topic);
+  }
+
+  EXPECT_EQ(recorder.Start(logName),
+            ignition::transport::log::RecorderError::NO_ERROR);
+
+  // Wait for the chirping to finish
+  testing::waitAndCleanupFork(chirper);
+
+  // Wait to make sure our callbacks are done processing the incoming messages
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  // Stop the recording so we can safely view the log
+  recorder.Stop();
+
+  ignition::transport::log::Log log;
+  EXPECT_TRUE(log.Open(logName));
+
+
+  using MsgType = ignition::transport::log::test::ChirpMsgType;
+  MsgType protoMsg;
+
+  std::string data;
+  std::string type;
+
+  for (const ignition::transport::log::Message &msg : log.QueryMessages())
+  {
+    data = msg.Data();
+    type = msg.Type();
+    EXPECT_EQ(protoMsg.GetTypeName(), type);
+  }
+
+  EXPECT_TRUE(protoMsg.ParseFromString(data));
+  EXPECT_EQ(numChirps, protoMsg.data());
+
+  ignition::common::removeFile(logName);
+}
+
+//////////////////////////////////////////////////
+void RecordPatternBeforeAdvertisement(const std::regex &_pattern)
+{
+  std::vector<std::string> topics = {"/foo1", "/foo2", "/bar1", "/bar2"};
+
+  int numMatchingTopics = 0;
+  for (const std::string &topic : topics)
+  {
+    if (std::regex_match(topic, _pattern))
+      ++numMatchingTopics;
+  }
+
+  EXPECT_NE(0, numMatchingTopics);
+
+  const std::string logName = IGN_TRANSPORT_LOG_BUILD_PATH"/test.log";
+  ignition::common::removeFile(logName);
+
+  ignition::transport::log::Recorder recorder;
+  recorder.AddTopic(_pattern);
+
+  EXPECT_EQ(recorder.Start(logName),
+            ignition::transport::log::RecorderError::NO_ERROR);
+
+  const int numChirps = 100;
+  testing::forkHandlerType chirper =
+      ignition::transport::log::test::BeginChirps(topics, numChirps);
+
+  // Wait for the chirping to finish
+  testing::waitAndCleanupFork(chirper);
+
+  // Wait to make sure our callbacks are done processing the incoming messages
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  // Stop recording so we can safely view the log
+  recorder.Stop();
+
+  ignition::transport::log::Log log;
+  EXPECT_TRUE(log.Open(logName));
+
+
+  auto VerifyTopic = [&](const std::string &_topic)
+  {
+    if (std::regex_match(_topic, _pattern))
+      return true;
+
+    std::cout << "Unexpected topic name: " << _topic << std::endl;
+    return false;
+  };
+
+  int64_t count = 0;
+
+  for (const ignition::transport::log::Message &msg : log.QueryMessages())
+  {
+    VerifyMessage(msg, count, numMatchingTopics, VerifyTopic);
+    ++count;
+  }
+
+  EXPECT_EQ(numChirps*numMatchingTopics, count);
+
+  ignition::common::removeFile(logName);
+}
+
+//////////////////////////////////////////////////
+TEST(recorder, BeginRecordingPatternBeforeAdvertisement)
+{
+  RecordPatternBeforeAdvertisement(std::regex(".*foo.*"));
+}
+
+//////////////////////////////////////////////////
+TEST(recorder, BeginRecordingAllBeforeAdvertisement)
+{
+  RecordPatternBeforeAdvertisement(std::regex(".*"));
+}
+
+//////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+  setenv(ignition::transport::log::SchemaLocationEnvVar.c_str(),
+         IGN_TRANSPORT_LOG_SQL_PATH, 1);
+
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
