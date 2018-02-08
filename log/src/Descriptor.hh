b7f12455eaac75ec7218caacef73037d955fc390
Michael X. Grey <grey@osrfoundation.org>
2018-02-08 10:35:35 -0800

    Doxygen fixes

--- log/src/Descriptor.hh
+++ log/src/Descriptor.hh
@@ -62,12 +62,13 @@ namespace ignition
         /// \internal Reset this descriptor. This should only be called by the
         /// Log class, when it is generating a new Descriptor after opening a
         /// new file.
+        /// \param[in] _topics The map of topics that the log contains.
         public: void Reset(const TopicKeyMap &_topics);
 
-        /// \internal \sa Descriptor::GetTopicsToMsgTypesToId()
+        /// \internal \sa Descriptor::TopicsToMsgTypesToId()
         public: NameToMap topicsToMsgTypesToId;
 
-        /// \internal \sa Descriptor::GetMsgTypesToTopicsToId()
+        /// \internal \sa Descriptor::MsgTypesToTopicsToId()
         public: NameToMap msgTypesToTopicsToId;
       };
     }
