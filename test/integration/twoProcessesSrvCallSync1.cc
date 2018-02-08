536df9eaaf1842bf4627cfaa7b097f945c5b967e
Carlos Aguero <caguero@osrfoundation.org>
2018-02-08 16:13:36 +0100

    Tweak tolerances

--- test/integration/twoProcessesSrvCallSync1.cc
+++ test/integration/twoProcessesSrvCallSync1.cc
@@ -70,7 +70,7 @@ TEST(twoProcSrvCallSync1, SrvTwoProcs)
 
   // Check if the elapsed time was close to the timeout.
   auto diff = std::max(elapsed, timeout) - std::min(elapsed, timeout);
-  EXPECT_LT(diff, 50);
+  EXPECT_LT(diff, 200);
 
   // Wait for the child process to return.
   testing::waitAndCleanupFork(pi);
