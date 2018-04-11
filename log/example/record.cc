7e569a2e80df337cf15874e8722280626e34f187
Michael X. Grey <grey@osrfoundation.org>
2018-04-11 12:02:55 -0700

    Simplify implementation of example_record

--- log/example/record.cc
+++ log/example/record.cc
@@ -20,29 +20,12 @@
 /// Launch the ignition-transport publisher example so this example has
 /// something to record.
 
-#include <signal.h>
-
-#include <chrono>
 #include <cstdint>
 #include <iostream>
 #include <regex>
-#include <thread>
-#include <condition_variable>
+
+#include <ignition/transport/Node.hh>
 #include <ignition/transport/log/Recorder.hh>
-
-using namespace std::chrono_literals;
-
-static bool g_stop = false;
-static std::condition_variable g_wakeup;
-
-void Interrupt(int _signal)
-{
-  if (SIGINT == _signal)
-  {
-    g_stop = true;
-    g_wakeup.notify_all();
-  }
-}
 
 //////////////////////////////////////////////////
 int main(int argc, char *argv[])
@@ -52,8 +35,6 @@ int main(int argc, char *argv[])
     std::cerr << "Usage: " << argv[0] << " OUTPUT.tlog\n";
     return -1;
   }
-
-  signal(SIGINT, Interrupt);
 
   ignition::transport::log::Recorder recorder;
 
@@ -72,11 +53,8 @@ int main(int argc, char *argv[])
   std::cout << "Press Ctrl+C to finish recording.\n  Recording... "
             << std::endl;
 
-  // Wait until the interrupt signal is sent. Waiting on a condition variable
-  // prevents the process from eating up unnecessary CPU cycles.
-  std::mutex mutex;
-  std::unique_lock<std::mutex> lock(mutex);
-  g_wakeup.wait(lock, [&](){ return g_stop; });
+  // Wait until the interrupt signal is sent.
+  ignition::transport::waitForShutdown();
 
   recorder.Stop();
 
