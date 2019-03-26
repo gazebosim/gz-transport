e3ead73b3be9da8737d44dea6f5ab96bccfd54d3
Carlos Agüero <caguero@osrfoundation.org>
2019-03-26 15:10:15 -0700

    Dates and install wget.

--- docker/build.bash
+++ docker/build.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-# Copyright (C) 2018 Open Source Robotics Foundation
+# Copyright (C) 2019 Open Source Robotics Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
