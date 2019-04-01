3b5ae38b5e0794612b5461f71505e293bc3cfcb5
Nate Koenig <natekoenig@gmail.com>
2019-04-01 12:44:07 -0700

    Start of C interface to transport

---
+++ include/ignition/transport/CIface.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef IGN_TRANSPORT_CIFACE_HH_
+#define IGN_TRANSPORT_CIFACE_HH_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  /// \brief Id of a transport node.
+  typedef int32_t IgnTransportNode;
+
+  /// \brief Initialize a transport node.
+  IgnTransportNode ignTransportInit(void);
+
+  /// \brief Publishe a message on a topic.
+  /// \param[in] _node Id of a node.
+  /// \param[in] _topic Topic on which to publish the message.
+  /// \param[in] _data Byte array of serialized data to publish.
+  /// \param[in] _msgType Name of the message type.
+  /// \return True on success.
+  bool ignTransportPublish(IgnTransportNode _node, const char *_topic,
+                         const void *_data, const char *_msgType);
+
+  /// \brief Subscribe to a topic, and register a callback.
+  /// \param[in] _node Id of a node.
+  /// \param[in] _topic Name of the topic.
+  /// \param[in] _callback The function to call when a message is received.
+  /// \return True on success.
+  bool ignTransportSubscribe(IgnTransportNode _node, const char *_topic,
+      void (*_callback)(const char *, const size_t, const char *));
+
+  /// \brief Block the current thread until a SIGINT or SIGTERM is received.
+  /// Note that this function registers a signal handler. Do not use this
+  /// function if you want to manage yourself SIGINT/SIGTERM.
+  void ignTransportWaitForShutdown();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/CIface.cc
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <memory>
+#include <vector>
+
+#include "ignition/transport/Node.hh"
+#include "ignition/transport/CIface.h"
+
+// All of the nodes.
+static std::vector<std::unique_ptr<ignition::transport::Node>> nodes;
+
+// Map of the publishers.
+// Key: pair of node Id and topic name. A node Id is generated from the
+// ignTransportInit function.
+// Value: A node publisher.
+static std::map<std::pair<IgnTransportNode, const char *>,
+  ignition::transport::Node::Publisher> publishers;
+
+/////////////////////////////////////////////////
+IgnTransportNode ignTransportInit(void)
+{
+  nodes.push_back(std::make_unique<ignition::transport::Node>());
+  return nodes.size() - 1;
+}
+
+/////////////////////////////////////////////////
+bool ignTransportPublish(IgnTransportNode _node, const char *_topic,
+                         const void *_data, const char *_msgType)
+{
+  if (_node >= 0 && _node < static_cast<int>(nodes.size()))
+  {
+    std::pair<IgnTransportNode, const char *> key(_node, _topic);
+
+    // Create a publisher if one does not exist.
+    if (publishers.find(key) == publishers.end())
+      publishers[key] = nodes[_node]->Advertise(_topic, _msgType);
+
+    // Publishe the message.
+    return publishers[key].PublishRaw(
+        reinterpret_cast<const char*>(_data), _msgType);
+  }
+
+  return false;
+}
+
+/////////////////////////////////////////////////
+bool ignTransportSubscribe(IgnTransportNode _node, const char *_topic,
+    void (*_callback)(const char *, const size_t, const char *))
+{
+  if (_node >= 0 && _node < static_cast<int>(nodes.size()))
+  {
+    return nodes[_node]->SubscribeRaw(_topic,
+        [_callback](const char *_msg,
+          const size_t _size,
+          const ignition::transport::MessageInfo &_info) -> void {
+        _callback(_msg, _size, _info.Type().c_str());
+        });
+  }
+
+  return false;
+}
+
+/////////////////////////////////////////////////
+void ignTransportWaitForShutdown()
+{
+  ignition::transport::waitForShutdown();
+}
