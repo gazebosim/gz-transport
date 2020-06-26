cc0b10c85ce9cf7c7218a2d38b667ec1710c81c6
John Shepherd <john@openrobotics.org>
2020-06-25 23:40:29 -0700

    Clean up
    Signed-off-by: John Shepherd <john@openrobotics.org>

--- log/src/Log_TEST.cc
+++ log/src/Log_TEST.cc
@@ -251,14 +251,9 @@ TEST(Log, NullDescriptorUnopenedLog)
 TEST(Log, OpenCorruptDatabase)
 {
   log::Log logFile;
-  /*
-  std::string transportLogPath = IGN_TRANSPORT_LOG_TEST_PATH;
-  std::string path = transportLogPath + "/data/state.tlog";
- */
   std::string path =
     testing::portablePathUnion(IGN_TRANSPORT_LOG_TEST_PATH, "data");
   path = testing::portablePathUnion(path, "state.tlog");
-
   logFile.Open(path);
   EXPECT_EQ(4806000000ns, logFile.EndTime());
 }
