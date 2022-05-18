979636467af8dd8a9c7dd7202dcac93275c0a593
methylDragon <methylDragon@gmail.com>
2022-05-17 23:01:27 -0700

    Implement deprecation trick
    Signed-off-by: methylDragon <methylDragon@gmail.com>

--- include/ignition/transport/config.hh
+++ include/ignition/transport/config.hh
@@ -15,4 +15,14 @@
  *
  */
 
+#ifndef IGNITION_TRANSPORT__CONFIG_HH_
+#define IGNITION_TRANSPORT__CONFIG_HH_
+
 #include <gz/transport/config.hh>
+
+namespace [[deprecated("Use gz namespace.")]]  ignition
+{
+  using namespace gz;
+}
+
+#endif
