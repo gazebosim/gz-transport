4282bc8f877349103141f9a379de08d0509cba85
Jorge Perez <jjperez@ekumenlabs.com>
2022-05-23 13:25:53 -0500

    Wrap gtest
    Signed-off-by: Jorge Perez <jjperez@ekumenlabs.com>

--- test/integration/authPubSub.cc
+++ test/integration/authPubSub.cc
@@ -28,12 +28,12 @@
 #pragma warning(pop)
 #endif
 
-#include "gtest/gtest.h"
 #ifdef _MSVC_VER
 #pragma warning(push)
 #pragma warning(disable: 4005)
 #pragma warning(disable: 4251)
 #endif
+#include "gtest/gtest.h"
 #include "ignition/transport/Node.hh"
 #include "ignition/transport/TransportTypes.hh"
 #include "ignition/transport/test_config.h"
