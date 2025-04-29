e289da8671d6a5493965a0fc13f667855365ab92
Ian Chen <ichen@openrobotics.org>
2025-04-29 23:06:59 +0000

    set ros env var
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- .github/ci/before_cmake.sh
+++ .github/ci/before_cmake.sh
@@ -1,1 +1,7 @@
-source /opt/ros/jazzy/setup.bash
+#!/bin/sh -l
+
+set -x
+
+ROS_DISTRO="jazzy"
+export LD_LIBRARY_PATH="/opt/ros/${ROS_DISTRO}/opt/zenoh_cpp_vendor/lib:$LD_LIBRARY_PATH"
+export CMAKE_PREFIX_PATH="/opt/ros/${ROS_DISTRO}/opt/zenoh_cpp_vendor/lib/cmake:$CMAKE_PREFIX_PATH"
