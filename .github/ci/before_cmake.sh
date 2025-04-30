1e5fdca1ccbe5c2c8261e956fd3d350cf67dad68
Ian Chen <ichen@openrobotics.org>
2025-04-30 02:26:04 +0000

    testing
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- .github/ci/before_cmake.sh
+++ .github/ci/before_cmake.sh
@@ -1,11 +1,12 @@
-#!/bin/sh -l
 
 set -x
-
 ROS_DISTRO="jazzy"
 #export LD_LIBRARY_PATH="/opt/ros/${ROS_DISTRO}/opt/zenoh_cpp_vendor/lib:$LD_LIBRARY_PATH"
 #export CMAKE_PREFIX_PATH="/opt/ros/${ROS_DISTRO}/opt/zenoh_cpp_vendor/lib/cmake:$CMAKE_PREFIX_PATH"
-ROS_SETUP_SCRIPT="/opt/ros/${ROS_DISTRO}/setup.sh"
+ROS_SETUP_SCRIPT="/opt/ros/${ROS_DISTRO}/setup.bash"
  if [ -f ${ROS_SETUP_SCRIPT} ]; then
+  set -a
   . ${ROS_SETUP_SCRIPT}
+  set +a
+  env | grep ROS
 fi
