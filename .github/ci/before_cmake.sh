ead63a979d81669af158d94f39409fa15d1a0524
Ian Chen <ichen@openrobotics.org>
2025-04-30 01:59:56 +0000

    testing setting cmake prefix path
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- .github/ci/before_cmake.sh
+++ .github/ci/before_cmake.sh
@@ -3,5 +3,6 @@
 set -x
 
 ROS_DISTRO="jazzy"
-export LD_LIBRARY_PATH="/opt/ros/${ROS_DISTRO}/opt/zenoh_cpp_vendor/lib:$LD_LIBRARY_PATH"
-export CMAKE_PREFIX_PATH="/opt/ros/${ROS_DISTRO}/opt/zenoh_cpp_vendor/lib/cmake:$CMAKE_PREFIX_PATH"
+#export LD_LIBRARY_PATH="/opt/ros/${ROS_DISTRO}/opt/zenoh_cpp_vendor/lib:$LD_LIBRARY_PATH"
+#export CMAKE_PREFIX_PATH="/opt/ros/${ROS_DISTRO}/opt/zenoh_cpp_vendor/lib/cmake:$CMAKE_PREFIX_PATH"
+. /opt/ros/${ROS_DISTRO}/setup.sh
