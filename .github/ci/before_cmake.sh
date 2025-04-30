2ee4b458b677889059cf7156a9bcc3ff6af47f13
Ian Chen <ichen@openrobotics.org>
2025-04-30 03:10:45 +0000

    cleanup
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- .github/ci/before_cmake.sh
+++ .github/ci/before_cmake.sh
@@ -1,12 +1,2 @@
-
-set -x
-ROS_DISTRO="jazzy"
-export ROS_DISTRO=${ROS_DISTRO}
-#export LD_LIBRARY_PATH="/opt/ros/${ROS_DISTRO}/opt/zenoh_cpp_vendor/lib:$LD_LIBRARY_PATH"
-#export CMAKE_PREFIX_PATH="/opt/ros/${ROS_DISTRO}/opt/zenoh_cpp_vendor/lib/cmake:$CMAKE_PREFIX_PATH"
-ROS_SETUP_SCRIPT="/opt/ros/${ROS_DISTRO}/setup.bash"
- if [ -f ${ROS_SETUP_SCRIPT} ]; then
-  set -a
-  . ${ROS_SETUP_SCRIPT}
-  set +a
-fi
+# Set ROS_DISTRO. This is needed for finding zenoh packages vendored by ROS 2
+export ROS_DISTRO="jazzy"
