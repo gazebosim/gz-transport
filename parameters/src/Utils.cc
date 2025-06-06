09c12e22eeb3d58a506b58ad83ecf80d9aa87821
Carlos Agüero <caguero@osrfoundation.org>
2025-06-06 19:02:32 +0200

    Clean up namespaces - part 4
    Signed-off-by: Carlos Agüero <caguero@osrfoundation.org>

--- parameters/src/Utils.cc
+++ parameters/src/Utils.cc
@@ -20,10 +20,13 @@
 #include <ios>
 #include <sstream>
 #include <string>
-using namespace gz;
+
+namespace gz::transport::parameters
+{
+// Inline bracket to help doxygen filtering.
+inline namespace GZ_TRANSPORT_VERSION_NAMESPACE {
 //////////////////////////////////////////////////
-std::optional<std::string>
-transport::parameters::getGzTypeFromAnyProto(
+std::optional<std::string> getGzTypeFromAnyProto(
   const google::protobuf::Any &_any)
 {
   auto typeUrl = _any.type_url();
@@ -38,3 +41,5 @@ transport::parameters::getGzTypeFromAnyProto(
   }
   return ret;
 }
+}  // namespace GZ_TRANSPORT_VERSION_NAMESPACE
+}  // namespace gz::transport::parameters
