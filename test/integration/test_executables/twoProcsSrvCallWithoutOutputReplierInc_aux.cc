456859c7db5c3ae7902e8815fa2ec187dc0e8c7f
Addisu Z. Taddese <addisu@openrobotics.org>
2025-08-12 23:01:33 -0500

    Properly terminate auxiliary processes when test exits
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- test/integration/test_executables/twoProcsSrvCallWithoutOutputReplierInc_aux.cc
+++ test/integration/test_executables/twoProcsSrvCallWithoutOutputReplierInc_aux.cc
@@ -16,8 +16,7 @@
 */
 #include <gz/msgs/int32.pb.h>
 
-#include <chrono>
-#include <climits>
+#include <limits>
 #include <string>
 
 #include "gz/transport/Node.hh"
@@ -30,7 +29,6 @@
 using namespace gz;
 
 static std::string g_topic = "/foo"; // NOLINT(*)
-static int kForever = INT_MAX;
 
 //////////////////////////////////////////////////
 /// \brief Provide a service without output.
@@ -45,8 +43,8 @@ void runReplier()
   transport::Node node;
   EXPECT_TRUE(node.Advertise(g_topic, srvWithoutOutput));
 
-  // Run the node forever. Should be killed by the test that uses this.
-  std::this_thread::sleep_for(std::chrono::milliseconds(kForever));
+  // Run the node until we get an input in stdin indicating we should exit
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max());
 }
 
 //////////////////////////////////////////////////
