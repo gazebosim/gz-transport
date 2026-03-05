2a39d2558126157b681e73b265a178e8c5410c9f
Steve Peters <scpeters@openrobotics.org>
2026-03-05 14:16:31 -0800

    Fix unused result warnings (#812)
    * Node::RequestRaw: check ParseFromString result
    * parameters Client: check PackFrom result
    * parameters Registry: check PackFrom result
    Also remove unneeded scope to simplify return logic.
    ---------
    
    Signed-off-by: Steve Peters <scpeters@intrinsic.ai>
    (cherry picked from commit 863876dce875400a80c331ee507d505443f94f57)
    
    # Conflicts:
    #	parameters/src/Registry.cc

--- parameters/src/Registry.cc
+++ parameters/src/Registry.cc
@@ -119,6 +119,7 @@ ParametersRegistry & ParametersRegistry::operator=(
 bool ParametersRegistryPrivate::GetParameter(const msgs::ParameterName &_req,
   msgs::ParameterValue &_res)
 {
+<<<<<<< HEAD
   {
     std::lock_guard guard{this->parametersMapMutex};
     auto it = this->parametersMap.find(_req.name());
@@ -126,8 +127,14 @@ bool ParametersRegistryPrivate::GetParameter(const msgs::ParameterName &_req,
       return false;
     }
     _res.mutable_data()->PackFrom(*it->second, "ign_msgs");
+=======
+  std::lock_guard guard{this->parametersMapMutex};
+  auto it = this->parametersMap.find(_req.name());
+  if (it == this->parametersMap.end()) {
+    return false;
+>>>>>>> 863876d (Fix unused result warnings (#812))
   }
-  return true;
+  return _res.mutable_data()->PackFrom(*it->second, "gz_msgs");
 }
 
 //////////////////////////////////////////////////
