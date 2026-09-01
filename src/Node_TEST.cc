10cbd1a2bd5d23a55142bd81555e006d68616b63
Luca Della Vedova <lucadv@intrinsic.ai>
2026-09-01 14:43:26 +0800

    Fix relative topic name resolution when doing duplicated advertise on Zenoh (#938)
    Signed-off-by: Luca Della Vedova <lucadv@intrinsic.ai>
    Co-authored-by: Carlos Agüero <cen.aguero@gmail.com>

--- src/Node_TEST.cc
+++ src/Node_TEST.cc
@@ -1197,6 +1197,58 @@ TEST(NodeTest, AdvertiseTwoEqualTopics)
 }
 
 //////////////////////////////////////////////////
+/// \brief Advertise the same topic with the same type twice on the same node.
+TEST(NodeTest, AdvertiseSameTopicSameTypeOnSameNode)
+{
+  transport::Node node;
+
+  auto pub1 = node.Advertise<msgs::Int32>(g_topic);
+  EXPECT_TRUE(pub1);
+  auto pub2 = node.Advertise<msgs::Int32>(g_topic);
+  EXPECT_FALSE(pub2);
+}
+
+//////////////////////////////////////////////////
+/// \brief Advertise a relative topic (no leading slash) twice on the same node.
+TEST(NodeTest, AdvertiseRelativeTopicTwice)
+{
+  transport::Node node;
+
+  auto pub1 = node.Advertise<msgs::Int32>("relative_topic");
+  EXPECT_TRUE(pub1);
+  auto pub2 = node.Advertise<msgs::Int32>("relative_topic");
+  EXPECT_FALSE(pub2);
+}
+
+//////////////////////////////////////////////////
+/// \brief Advertise a remapped topic twice on the same node.
+TEST(NodeTest, AdvertiseRemappedTopicTwice)
+{
+  transport::NodeOptions opts;
+  opts.AddTopicRemap("/remap_from", "/remap_to");
+  transport::Node node(opts);
+
+  auto pub1 = node.Advertise<msgs::Int32>("/remap_from");
+  EXPECT_TRUE(pub1);
+  auto pub2 = node.Advertise<msgs::Int32>("/remap_from");
+  EXPECT_FALSE(pub2);
+}
+
+//////////////////////////////////////////////////
+/// \brief Advertise a namespaced relative topic twice on the same node.
+TEST(NodeTest, AdvertiseNamespacedTopicTwice)
+{
+  transport::NodeOptions opts;
+  opts.SetNameSpace("my_ns");
+  transport::Node node(opts);
+
+  auto pub1 = node.Advertise<msgs::Int32>("ns_topic");
+  EXPECT_TRUE(pub1);
+  auto pub2 = node.Advertise<msgs::Int32>("ns_topic");
+  EXPECT_FALSE(pub2);
+}
+
+//////////////////////////////////////////////////
 /// \brief Use two threads using their own transport nodes. One thread
 /// will publish a message, whereas the other thread is subscribed to the topic.
 TEST(NodeTest, PubSubTwoThreadsSameTopic)
